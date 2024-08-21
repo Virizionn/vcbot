45e662f94e1fbf9f2c2fdcdfd46f50dbb9af82de
Mark <markinoe@gmail.com>
2024-08-20 21:39:58 -0700

    Scraper implementation
    - Wrote functions for update_posts to scrape posts from a url and update the mongo database
    - Improvements to database.py: read/write game attributes, decorator deterring erroneous _game_ entries in function calls

--- database.py
+++ database.py
@@ -1,10 +1,25 @@
 from object_types import vote, post, phase
 
 import pymongo
+from functools import wraps
+
+from functools import wraps
+
+# Define the decorator
+def validate_game(func):
+    @wraps(func)
+    def wrapper(game, *args, **kwargs):
+        allowed_games = {'A', 'B', 'C'}
+        if game not in allowed_games:
+            raise ValueError(f"Invalid game: {game}. Must be one of {allowed_games}")
+        return func(game, *args, **kwargs)
+    return wrapper
+
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mafia"]
 
+@validate_game
 def add_vote_to_db(vote, game):
     col = db["votes"]
     myquery = { "postnum": vote.postnum, "game": game }
@@ -12,21 +27,31 @@ def add_vote_to_db(vote, game):
     col.update_one(myquery, newvalues, upsert=True)
     return
 
+@validate_game
 def get_votes_by_range(game, start, end):
     col = db["votes"]
     res = col.find({ "game": game, "postnum": { "$gte": start, "$lte": end } })
     return list(res)
 
+@validate_game
+def get_all_votes(game):
+    col = db["votes"]
+    res = col.find({ "game": game })
+    return list(res)
+
+@validate_game
 def get_votes_by_voter(game, player):
     col = db["votes"]
     res = col.find({ "game": game, "voter": player })
     return list(res)
 
+@validate_game
 def get_votes_by_target(game, player):
     col = db["votes"]
     res = col.find({ "game": game, "target": player })
     return list(res)
 
+@validate_game
 def add_post_to_db(post, game):
     col = db["posts"]
     myquery = { "post_id": post.id }
@@ -34,11 +59,19 @@ def add_post_to_db(post, game):
     col.update_one(myquery, newvalues, upsert=True)
     return
 
+@validate_game
 def get_posts_by_authors(game, authors):
     col = db["posts"]
     res = col.find({ "game": game, "author": { "$in": authors } })
     return list(res)
 
+@validate_game
+def get_all_posts(game):
+    col = db["posts"]
+    res = col.find({ "game": game })
+    return list(res)
+
+@validate_game
 def add_phase_to_db(phase, game):
     col = db["phases"]
     myquery = { "postnum": phase.postnum, "game": game }
@@ -53,19 +86,32 @@ def add_alias_to_db(name, alias):
     col.update_one(myquery, newvalues, upsert=True)
     return
 
-# In addition to this, I'll need a function to update database keys. Should be similar to updateData from the replit code.
-def update_key_in_db(key, value):
-    col = db["data"]
-    q = { "key": key }
-    col.delete_one(q)
-    col.insert_one({ "key": key, "value": value })
+@validate_game
+def set_game_attr(game, key, value):
+    #game is the column, key is the row, value is the value to update
+    col = db["game_attr"]
+    myquery = { "game": game, "key": key }
+    newvalues = { "$set": { "game": game, "key": key, "value": value } }
+    col.update_one(myquery, newvalues, upsert=True)
     return
 
-# I'll also need a function to get data from the database.
-def get_data_from_db(key):
-    col = db["data"]
-    q = { "key": key }
+@validate_game
+def get_game_attr(game, key):
+    col = db["game_attr"]
+    q = { "game": game, "key": key }
     res = col.find_one(q)
     if res is None:
         return None
     return res["value"]
+
+@validate_game
+def wipe_game_db(game):
+    #wipe all posts for a certain game
+    col = db["posts"]
+    q = { "game": game }
+    col.delete_many(q)
+    #wipe all votes for a certain game
+    col = db["votes"]
+    q = { "game": game }
+    col.delete_many(q)
+    return
